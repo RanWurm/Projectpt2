f4917caec25e985bf467275570db95a67c1f58bf
RanWurm <ranwurembrand@gmail.com>
2024-02-03 18:36:27 +0200

    minor changes

--- FakeBook/src/app/App.js
+++ FakeBook/src/app/App.js
@@ -1,8 +1,8 @@
-import './App.css';
 import FeedPage from '../pages/FeedPage';
 function App() {
   return (
     <div>
+      <header><title>sdas</title></header>
       <FeedPage></FeedPage>
     </div>
   );
