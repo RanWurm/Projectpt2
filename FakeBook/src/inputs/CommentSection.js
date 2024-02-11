6778a70f4f5bfc9843ff1503b35bb7587066cbad
RanWurm <ranwurembrand@gmail.com>
2024-02-11 11:42:42 +0200

    comment fixed

---
+++ FakeBook/src/inputs/CommentSection.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../css/inputsCss/Comment.css';
+
+
+
+const Comment = ({commentList,issShow }) => {
+  if (commentList===0 || issShow === false){
+    return(<div></div>);
+  }
+  return (
+    <div>
+      <ul>
+        {/* Render the list items */}
+        {commentList.map((text, index) => (
+          <div className="comment" key={index}>{text}</div>
+        ))}
+       </ul>
+        </div>
+  );
+};
+
+export default Comment;
--- FakeBook/src/inputs/Post.js
+++ FakeBook/src/inputs/Post.js
@@ -7,21 +7,42 @@ import CommentButton from '../buttons/CommentButton';
 import comment_icon from '../res/comment_icon.png'
 import Button from '../buttons/Button';
 import redButton from '../res/redButton.png'
-import Comment from './Comment';
+import CommentSection from './CommentSection';
+import Send from '../res/send_button.png'
+
 
 function Post  ({ author, icon, content, postId }) {
   const [likeCount, setLikeCount] = useState(0);
-
+  const [inputText, setInputText] = useState('');
+  const [textList, setTextList] = useState([]);
+  const [showPosts,setShowPosts] = useState(false);
+  
+  const handleShowpost = () =>{
+    setShowPosts(!showPosts);
+  };
+  
   const handleLikeClick = () => {
     // Increment like count when the like button is clicked
     setLikeCount(likeCount + 1);
   };
   
-  
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  // Function to handle adding text to the list
+  const handleAddToList = () => {
+    // Check if inputText is not empty
+    if (inputText.trim() !== '') {
+      // Add inputText to the list
+      setTextList([...textList, inputText]);
+      // Clear the inputText
+      setInputText('');
+    }
+  };
 
 
-  return (
-    
+  return ( 
     <div className="post">
       <div className="post-header">
         <img
@@ -37,7 +58,7 @@ function Post  ({ author, icon, content, postId }) {
         <span className='right_col'>
          {likeCount}
         <img className = "interaction-icon" src ={like_icon} alt='' /> 
-         {likeCount}
+         {textList.length}
         <img className = "interaction-icon" src ={comment_icon} alt='' />
         </span>
       </span> 
@@ -47,10 +68,10 @@ function Post  ({ author, icon, content, postId }) {
       <div className='interactions-section'>
         <div className='post-buttons-bar'>
           <div className='post_left_col'>
-          <LikeButton className onClick={handleLikeClick} />
+          <LikeButton  onClick={handleLikeClick} />
           </div>
           <div className='post_center_col'>
-             <CommentButton/>
+             <CommentButton onClick={handleShowpost} />
           </div>
           <div className='post_right_col'>
             <Button id ={"interaction-button"} icon={redButton} meme = {"yes"}></Button>
@@ -58,12 +79,25 @@ function Post  ({ author, icon, content, postId }) {
         </div>
       </div>
         
-      <div className="post-content" >
+      <div className="comment-section" >
           <div className="addCommentContainer">
      
-            <Comment/>
-          </div>
-          <div className="listOfComments"></div>
+       <div className="comment-section">
+        <CommentSection commentList={textList} issShow={showPosts}/>
+       <input 
+       className = "comment-section" 
+       type = "text" 
+       placeholder='add comment'
+        value={inputText}
+        onChange={handleInputChange}
+      />
+      <div className='left-col'>
+      <button className='send-button' onClick={handleAddToList}>
+      <img className='send-button-logo' src={Send} alt="" />
+      </button>  
+      </div>
+      </div>
+      </div> 
      </div>
     </div>
   );
