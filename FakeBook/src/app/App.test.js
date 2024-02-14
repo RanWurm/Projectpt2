c809624196196b4c00132ff5a8e5ed64018a6225
Hillel0 <hmr2000@gmail.com>
2024-02-14 15:04:47 +0200

    Finished tests

--- FakeBook/src/app/App.test.js
+++ FakeBook/src/app/App.test.js
@@ -1,16 +1,7 @@
-import { render, fireEvent } from '@testing-library/react';
-import Button from './Button';
-import App from '..';
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-describe('Button Component', () => {
-  it('should open the meme video when the button is clicked and meme is true', () => {
-    const openSpy = jest.spyOn(window, 'open');
-    const { getByAltText } = render(<Button id="test-button" icon="test-icon.png" meme="true" />);
-    const button = getByAltText('Button Icon');
-    fireEvent.click(button);
-    expect(openSpy).toHaveBeenCalledWith('https://www.youtube.com/watch?v=IEKeg_2oJFI', '_blank');
-    openSpy.mockRestore();
-  });
-
-  // other tests go here
+test('renders without throwing an error', () => {
+  render(<App/>);
 });
--- FakeBook/src/buttons/Button.test.js
+++ FakeBook/src/buttons/Button.test.js
@@ -1,3 +1,23 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils'; // Import act from react-dom/test-utils
 import Button from './Button';
+
+it('renders a button with the correct meme prop', () => {
+  const div = document.createElement('div');
+  act(() => {
+    ReactDOM.render(<Button meme="no" />, div);
+  });
+
+  // Find the rendered button element
+  const button = div.querySelector('button');
+
+  // Check if the button exists
+  expect(button).not.toBeNull();
+
+  // Check if the meme prop is correctly applied to the button
+  expect(button.getAttribute('meme')).toEqual(null);
+
+  // Unmount the component
+  ReactDOM.unmountComponentAtNode(div);
+});
