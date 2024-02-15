6d45347331961a47e7da25f5c3e986b426ab142c
RanWurm <ranwurembrand@gmail.com>
2024-02-15 11:02:50 +0200

    need to finish darkmode

--- FakeBook/src/Bars/NavBar.js
+++ FakeBook/src/Bars/NavBar.js
@@ -2,14 +2,14 @@ import React from 'react'
 import SearchTab from '../inputs/SearchTab';
 import '../css/BarsCss/NavBar.css'
 
-export default function NavBar({logo,firstHandleClick,secHandleClick}) {
+export default function NavBar({logo,firstHandleClick}) {
   return (
 	<div className = "navbar">
 	<div className="left_col">
 			<img className = "fb_icon " src = {logo}/>
 			<SearchTab id = {"tab"}></SearchTab>
 			<button className='button' onClick={firstHandleClick}>log-out</button>
-			<button className='button' onClick={secHandleClick}>dark-mode</button>
+			<button className='button' >dark-mode</button>
 	</div>
 	</div>
   );
